cfcd3a34ac5af906854f067c882f05444b4da8d9
szatsepa <arcady.1254@gmail.com>
2012-07-06 22:45:45 +0300

    faq bg & med bg

--- js/faq.js
+++ js/faq.js
@@ -7,11 +7,7 @@ $(document).ready(function () {
     if (document.readyState != "complete"){
 		setTimeout( arguments.callee, 100 );
 		return;
-	}
-    
-//   $("#main_0").css({'background-image': "url('../images/faq_bg.png')"});
-//   
-//   $("#prelo").css('display','none');   
+	}  
         
 });
 
